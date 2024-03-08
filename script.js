d375bbd829d2b372c15b2995e9f418c76b838b02
Mushroom1983 <151859550+Mushroom1983@users.noreply.github.com>
2024-03-08 11:16:42 +0000

    added api and query selectors

---
+++ script.js
@@ -0,0 +1,17 @@
+var searchForm = document.querySelector("#search-form");
+
+var searchInput = document.querySelector("#search-input");
+
+var apiKey = a67dededc95f92bf05c1b3a0e3e9c121
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ searchInput + "&" + "appid="+ apiKey;
+
+$("#search-button").on("click", function(event){
+    event.preventDefault();
+    
+    console.log(searchInput.value);
+
+    if (searchInput.value === "" || searchInput.value === null) {
+        return;
+    }
+})
