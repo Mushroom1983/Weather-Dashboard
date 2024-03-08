8f4016d24d2394ed6a67925fea3d0113ab0f4c0f
Mushroom1983 <151859550+Mushroom1983@users.noreply.github.com>
2024-03-08 12:53:59 +0000

    fixed date issue, created temp issue!

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,144 @@
 var searchForm = document.querySelector("#search-form");
-
 var searchInput = document.querySelector("#search-input");
+var apiKey = "a67dededc95f92bf05c1b3a0e3e9c121";
+var historyList = $("#history");
 
-var apiKey = "a67dededc95f92bf05c1b3a0e3e9c121"
+$("#search-button").on("click", function (event) {
+  event.preventDefault();
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ searchInput + "&" + "appid="+ apiKey;
+  if (searchInput.value === "" || searchInput.value === null) {
+    return;
+  }
 
-$("#search-button").on("click", function(event){
-    event.preventDefault();
-    
-    console.log(searchInput.value);
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchInput.value +
+    "&appid=" +
+    apiKey;
 
-    if (searchInput.value === "" || searchInput.value === null) {
-        return;
-    }
-})
+  $("#history").append(
+    "<button class='btn btn-secondary history-button'>" +
+      searchInput.value +
+      "</button> "
+  );
+  searchInput.value = "";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ searchInput + "&" + "appid="+ apiKey;
+  var buttonCount = $(".history-button").length;
 
-$("#history").append("<button>" + searchInput.value + "</button>");
+  // Limit the number of buttons to 6
+  if (buttonCount >= 8) {
+    // Remove the oldest button
+    $(".history-button:first").remove();
+  }
 
-console.log(queryURL); // This is for testing purposes.
+  // if a button is pressed search for that city
+  $(".history-button").on("click", function () {
+    var city = $(this).text();
+    queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&" +
+      "appid=" +
+      apiKey;
 
-$("#history-button").on("click", function() {
-    var today = day.js().format("DD/MM/YYYY");
-    var iconData = $(this).attr("data-icon");
-    var iconURL = "https://openweathermap.org/img/wn/" + iconData + "@2x.png";
-    var tempData = $(this).attr("data-temp");
-    var humidityData = $(this).attr("data-humidity");
-    var windData = $(this).attr("data-wind");
-    var uvData = $(this).attr("data-uv");
-})
-
-fetch(queryURL)
-    .then(function(response) {
+    fetch(queryURL)
+      .then(function (response) {
         return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
+      })
+      .then(function (data) {
+        if (data.weather && data.weather.length > 0) {
+          var today = dayjs().format("DD MM, YYYY");
+          var iconCode = data.weather[0].icon;
+          var iconUrl =
+            "https://openweathermap.org/img/w/" + iconCode + ".png";
 
-        if (data.cod === 404) {
-            $("#today").text("City not found. Please try again.");
-            return;
+          $("#today").empty(); // Clear previous content
+          $("#today").append(
+            "<h1>" +
+              data.name +
+              " " +
+              today +
+              "<img src='" +
+              iconUrl +
+              "'>" +
+              "</h1>"
+          );
+
+          $("#today").append("<p>Temp:" + data.main.temp + "</p>");
+          $("#today").append("<p>Wind:" + data.wind.speed + "</p>");
+          $("#today").append("<p>Humidity:" + data.main.humidity + "</p>");
+
+          console.log(data);
+
+          $("#forecast").empty();
+          $("#forecast").append("<h1>" + "5 Day Forecast" + "</h1>");
+
+          const forecast =
+            "https://api.openweathermap.org/data/2.5/forecast?q=" +
+            city +
+            "&" +
+            "appid=" +
+            apiKey;
+
+          return fetch(forecast);
+        } else {
+          console.log("Invalid response data format");
         }
-    })
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (forecastData) {
+        const list = forecastData.list;
+
+        for (let i = 7; i < list.length; i += 8) {
+          const { dt_txt, weather, main, wind } = list[i];
+          const date = dt_txt.split(" ")[0].replace(/-/g, "/");
+          const revDate = reverseDate(date); 
+          const temp = main.temp;
+          const celcius = temp - 273.15;
+          const iconCode = weather[0].icon;
+          const iconUrl =
+            "https://openweathermap.org/img/w/" + iconCode + ".png";
+
+          var cardHtml =
+            "<div class='card'>" +
+            "<div class='card-body'>" +
+            "<h5 class='card-title'>" +
+            "Date" +
+            revDate + // Use revDate instead of data.dt
+            "<img src='" +
+            iconUrl +
+            "'>" +
+            "</h5>" +
+            "<p class='card-text'>" +
+            "Temp" +
+            celcius + // Use celcius instead of data.main.temp
+            "</p>" +
+            "<p class='card-text'>" +
+            "Wind" +
+            wind.speed + // Use wind.speed instead of data.wind.speed
+            "</p>" +
+            "<p class='card-text'>" +
+            "Humidity" +
+            main.humidity + // Use main.humidity instead of data.main.humidity
+            "</p>" +
+            "</div>" +
+            "</div>";
+
+          $("#forecast").append(cardHtml);
+          $(".card").css("width", "18%");
+          $(".card").css("margin", "2px");
+          $(".card").css("background-color", "darkgray");
+        }
+      })
+      .catch(function (error) {
+        console.log("Error fetching forecast data:", error);
+      });
+  });
+});
+
+function reverseDate(date) {
+  const [year, month, day] = date.split("/");
+  return day + "/" + month + "/" + year;
+}
